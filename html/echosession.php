d2068802b01f78df2d6346e68c84154361c206f8
GuanxiongWu <bryantxiong@gmail.com>
2015-04-05 14:54:39 -0600

    delete

--- html/echosession.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-  session_name('Private');
-  session_start();
-  print_r($_SESSION);
-?>
--- html/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-  session_name('Private'); 
-  session_start();
-  session_unset();
-  session_write_close();
-  session_destroy();
-  header('Location: ./page_signin.php');
-  exit;
-?>
